a92d54406c2451faf0c433f17bd3b8d4143f0a1f
Leonardo Cabral <leomcabral@gmail.com>
2022-01-09 13:48:04 -0300

    [improvement] Maven Search by group and artifact
    Changes to `ms.sh` to be able to search Maven by group and artifact.
    To search using group and artifact, just add a colon (:) between the
    search terms.
    
    Ex.:
    ms.sh org.apache.logging.log4j:log4j

--- ms.sh
+++ ms.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-
 CURL_CMD="curl -skL "
 URL_SEARCH="https://search.maven.org/solrsearch/select?"
 
-QUERY=$1
+QUERY="${1:-}"
+
+if [ -z "$QUERY" ]; then
+    echo "Provide a query string to search maven central" 1>&2
+    exit 127
+fi
+
+IFS=':' read -ra VAL <<< "$QUERY"
+GROUP="${VAL[0]}"
+ARTIFACT=${VAL[1]:-}
+
+if [ -n "$ARTIFACT" ]; then
+    QUERY="g:${GROUP}%20AND%20a:${ARTIFACT}"
+fi
+
 RESULT=$($CURL_CMD "${URL_SEARCH}q=${QUERY}")
 
 # Format result
